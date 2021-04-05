ab0a66cfb6733148a9733fb1a5cc5bd49d1fd133
farmijo <farmijo16@gmail.com>
2021-04-05 22:18:10 +0200

    Cleaning main

--- src/main/java/com/example/patterns/Main.java
+++ src/main/java/com/example/patterns/Main.java
@@ -5,10 +5,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
         var sandwich = new Sandwich.Builder(1).cheeseSlices(2).build();
-        System.out.print(sandwich);
-        System.out.println("eoooeooe");
     }
 }
 
